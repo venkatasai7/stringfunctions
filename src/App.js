8dc9d328fc96dfe3e930cb7979368960044a6466
Venkata Sai Kuniganti <venkatasai7677@gmail.com>
2024-05-29 10:22:44 +0530

    stringfunctions

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+
+
+export default function App() {
+  return (
+    <>
+     <Navbar title = "String Functions" />
+          <div className="container my-5 ">
+          <TextForm heading = "Enter your text here below"/>
+          </div>
+
+          <div className="App">
+      <header className="App-header">
+  
+      </header>
+    </div>
+     
+
+     
+
+
+          
+    </>
+  );
+}
+
+
+;
---
+++ src/Components/TextForm.js
@@ -0,0 +1,73 @@
+import React,{useState} from 'react'
+
+
+
+
+export default function TextForm(props) {
+    const [text, setText] = useState("enter here..");
+   
+    const HandleUpperCaseClick = () =>
+        {
+            console.log("upper case was clicked");
+            let outtext = text.toUpperCase();
+            setText(outtext);
+            
+        };
+
+        const HandleLowerCaseClick = () =>
+            {
+                console.log("Lower case was clicked");
+                let outtext = text.toLowerCase();
+                setText(outtext);
+                
+            };
+
+    const HandleOnChange = (event) => {
+            console.log("on change")
+            setText(event.target.value);
+    };
+
+
+    const CopyToClipBoard = (event) =>
+        {
+        const textArea = document.createElement("textarea");
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.select();
+        try {
+            document.execCommand('copy');
+            alert("Copy successful!");
+        } catch (err) {
+            console.log("Copy failed: " + err);
+        }
+        document.body.removeChild(textArea);
+
+        };
+
+        const OnClear = (event) =>
+            {
+                setText("");
+            };
+
+    return (
+    <>
+    <div>
+        <div className="mb-3">
+            <h2>{props.heading}</h2>
+            <textarea className="form-control"  value={text} onChange={HandleOnChange} id="MyBox" rows="6"></textarea>
+        </div>
+        <button className="btn btn-primary mx-3" onClick ={HandleUpperCaseClick}> Convert to Uppercase</button>
+        <button className="btn btn-primary mx-3" onClick ={HandleLowerCaseClick}> Convert to Lowercase</button>
+
+        <button className="btn btn-primary mx-3" onClick ={CopyToClipBoard}> Copy Text</button>
+        <button className="btn btn-primary mx-3" onClick ={OnClear}> Clear</button>
+    </div>
+    <div className="container my-2">
+        <h2>Your Text Summary</h2>
+        <p> {text.split(" ").length} words , {text.length} characters.</p>
+        
+
+    </div>
+    </>
+  )
+}
